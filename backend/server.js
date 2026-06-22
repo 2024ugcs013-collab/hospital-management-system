4717d926c2bc9879f53dd7db4c969941cb22d5ea
2024ugcs013-collab <2024ugcs013@nitjsr.ac.in>
2026-06-22 17:00:25 +0530

    Setup frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
-import dotenv from 'dotenv';
-import app from './src/app.js';
+import express from "express";
 
-dotenv.config();
 
-const port = process.env.PORT || 5000;
+const app = express();
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.get("/", (req, res) => {
+  res.send("Hospital Management API Running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
